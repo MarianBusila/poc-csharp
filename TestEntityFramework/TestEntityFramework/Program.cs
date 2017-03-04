287ba9152d74353724c8970e17e9429f1f7dcb92
Marian Busila <marian@imposition.com>
2017-03-04 10:55:51 -0500

    EntityFramework

---
+++ TestEntityFramework/TestEntityFramework/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RecepiesEntities context = new RecepiesEntities())
+            {
+                //insert receipe
+                /*
+                Receipe receipe = new Receipe { Name = "Chicken Salad" };
+                context.Receipes.Add(receipe);
+                context.SaveChanges();
+                */
+
+                //select receipe
+                /*
+                Receipe reciepe = context.Receipes.FirstOrDefault(r => r.Name == "Chicken Salad");
+                Console.WriteLine(reciepe.Id);
+                */
+
+                //update receipe
+                /*
+                Receipe reciepe = context.Receipes.FirstOrDefault(r => r.Name == "Chicken Salad");
+                reciepe.Name = "Burger";
+                context.SaveChanges();
+                */
+
+                //delete receipe
+                /*
+                Receipe reciepe = context.Receipes.FirstOrDefault(r => r.Name == "Chicken Salad");
+                context.Receipes.Remove(reciepe);
+                context.SaveChanges();
+                */
+
+                //insert category
+                /*
+                context.Categories.Add(new Category { Name = "Breakfast" });
+                context.Categories.Add(new Category { Name = "Lunch" });
+                context.SaveChanges();
+                */
+
+                //1. Using Id properties
+                /*
+                Category category = context.Categories.FirstOrDefault(c => c.Name == "Breakfast");
+                context.Receipes.Add(new Receipe { Name = "Cereal", CategoryId = category.Id });
+                context.SaveChanges();
+                */
+
+                //2. Receipe.Category navigation property
+                /*                
+                Category category = context.Categories.FirstOrDefault(c => c.Name == "Lunch");
+                context.Receipes.Add(new Receipe { Name = "Pizza", Category = category });
+                context.SaveChanges();
+                */
+
+                //3. Category.Receipies navigation property
+                /*          
+                Category category = context.Categories.FirstOrDefault(c => c.Name == "Lunch");
+                category.Receipes.Add(new Receipe { Name = "Soup" });
+                context.SaveChanges();
+                */
+
+                Category category = context.Categories.FirstOrDefault(c => c.Name == "Lunch");
+                List<Receipe> recepies = category.Receipes.ToList();
+                recepies.ForEach(r => Console.WriteLine(r.Name));
+
+
+
+
+
+            }
+
+        }
+    }
+}
