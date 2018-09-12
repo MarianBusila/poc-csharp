1c6c7e33948ab760a5afa611e6febad946f37b58
Marian Busila <marian.busila@gmail.com>
2018-09-12 17:14:55 -0400

    Jaeger - HelloWorld - Span example

---
+++ JaegerHelloWorld/JaegerHelloWorld/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using OpenTracing;
+using OpenTracing.Util;
+
+using Library;
+
+namespace JaegerHelloWorld
+{
+    internal class Hello
+    {
+        private readonly ITracer _tracer;
+        private readonly ILogger<Hello> _logger;
+
+        public Hello(ITracer tracer, ILoggerFactory loggerFactory)
+        {
+            _tracer = tracer;
+            _logger = loggerFactory.CreateLogger<Hello>();
+        }
+
+        void SayHello(string helloTo)
+        {
+            var span = _tracer.BuildSpan("say-hello").Start();
+            var helloString = $"Hello, {helloTo}";
+            _logger.LogInformation(helloString);
+            span.Finish();
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentException("Expecting one argument");
+            }
+
+            using (var loggerFactory = new LoggerFactory().AddConsole())
+            {
+                var tracer = Tracer.InitTracer("hello-world", loggerFactory);
+                var helloTo = args[0];
+                new Hello(tracer, loggerFactory).SayHello(helloTo);
+            }
+
+        }
+    }
+}
---
+++ JaegerHelloWorld/Library/Tracer.cs
@@ -0,0 +1,25 @@
+﻿using Jaeger;
+using Jaeger.Samplers;
+using Microsoft.Extensions.Logging;
+using OpenTracing;
+
+namespace Library
+{
+    public class Tracer
+    {
+        public static ITracer InitTracer(string serviceName, ILoggerFactory loggerFactory)
+        {
+            var samplerConfiguration = new Configuration.SamplerConfiguration(loggerFactory)
+                .WithType(ConstSampler.Type)
+                .WithParam(1);
+
+            var reporterConfiguration = new Configuration.ReporterConfiguration(loggerFactory)
+                .WithLogSpans(true);
+
+            return new Configuration(serviceName, loggerFactory)
+                .WithSampler(samplerConfiguration)
+                .WithReporter(reporterConfiguration)
+                .GetTracer();
+        }
+    }
+}
