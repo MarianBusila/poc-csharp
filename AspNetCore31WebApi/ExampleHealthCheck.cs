360cafc168c0ef623b51303461ccd92f7c1f7234
Marian Busila <mbusila@pfdrive.com>
2020-11-15 13:28:21 -0500

    add basic health checks

---
+++ AspNetCore31WebApi/ExampleHealthCheck.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNetCore31WebApi
+{
+    public class ExampleHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var healthCheckResultHealthy = true;
+            
+            if(healthCheckResultHealthy)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy("A healthy result.", new Dictionary<string, object> { { "key", "value" } }));
+            }
+
+            return Task.FromResult(HealthCheckResult.Unhealthy("Un unhealthy result.", new Exception("Status check failed because of an exception")));
+        }
+    }
+}
--- AspNetCore31WebApi/Startup.cs
+++ AspNetCore31WebApi/Startup.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -25,6 +28,10 @@ namespace AspNetCore31WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            // all health checks have to be ok, so that the overall status is ok
+            services.AddHealthChecks()
+                .AddCheck<ExampleHealthCheck>("example_health_check")
+                .AddCheck("example_health_check_lambda", () => HealthCheckResult.Healthy("Example is ok"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +49,15 @@ namespace AspNetCore31WebApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+                {
+                    ResultStatusCodes =
+                        {
+                            [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                            [HealthStatus.Degraded] = StatusCodes.Status200OK,
+                            [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+                        }
+                });
             });
         }
     }
