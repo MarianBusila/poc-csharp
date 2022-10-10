827c3186e23d2eb8691d236f0713cea3e4a8a2b7
Marian Busila <mbusila@pfdrive.com>
2022-10-10 16:58:55 -0400

    add platorm repository and seed data

--- Microservices/PlatformService/Program.cs
+++ Microservices/PlatformService/Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("InMem");
+});
+
+builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,5 +31,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+PrepDb.PrepPopulation(app);
 
 app.Run();
