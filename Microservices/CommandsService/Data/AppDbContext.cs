297eda09bd6ca5755053752c6a2cd2376fa971c2
Marian Busila <mbusila@pfdrive.com>
2022-10-12 17:26:02 -0400

    create command repository

---
+++ Microservices/CommandsService/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CommandsService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Command> Commands { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Platform>()
+                .HasMany(p => p.Commands)
+                .WithOne(c => c.Platform!)
+                .HasForeignKey(c => c.Platform!.Id);
+
+            modelBuilder.Entity<Command>()
+                .HasOne(c => c.Platform)
+                .WithMany(p => p.Commands)
+                .HasForeignKey(c => c.PlatformId);
+        }
+    }
+}
