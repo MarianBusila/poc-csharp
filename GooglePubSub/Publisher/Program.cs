44c58d34f743582b10ae70cf2dc097e1e278204c
mbusila-unity3d <marian.busila@unity3d.com>
2023-08-12 13:55:44 -0400

    publish and subscribe to pull messages

--- GooglePubSub/Publisher/Program.cs
+++ GooglePubSub/Publisher/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Google.Cloud.PubSub.V1;
 
-Console.WriteLine("Hello, World!");
+string projectId = "unity-solutions-pwest-test";
+string topicId = "test-marian";
+
+// Create topic
+PublisherServiceApiClient publisherServiceApiClient = await PublisherServiceApiClient.CreateAsync();
+TopicName topicName = new TopicName(projectId, topicId);
+
+try {
+    Console.WriteLine("Creating topic ...");
+    publisherServiceApiClient.CreateTopic(topicName);
+    Console.WriteLine("Topic created");
+}
+catch (Grpc.Core.RpcException)
+{
+    Console.WriteLine("Topic already exists");
+}
+
+// Publish a message to the topic
+PublisherClient publisher = await PublisherClient.CreateAsync(topicName);
+string messageId = await publisher.PublishAsync("Hello at " + DateTime.Now);
+Console.WriteLine("Published MessageId: " + messageId);
+await publisher.ShutdownAsync(TimeSpan.FromSeconds(5));
--- GooglePubSub/Subsriber/Program.cs
+++ GooglePubSub/Subsriber/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Google.Cloud.PubSub.V1;
 
-Console.WriteLine("Hello, World!");
+string projectId = "unity-solutions-pwest-test";
+string topicId = "test-marian";
+string subscriptionId = "test-marian-subscription";
+
+// Create subscription
+SubscriberServiceApiClient subscriberServiceApiClient = await SubscriberServiceApiClient.CreateAsync();
+TopicName topicName = new TopicName(projectId, topicId);
+SubscriptionName subscriptionName = new SubscriptionName(projectId, subscriptionId);
+PushConfig? pushConfig = null;
+int ackDeadlineSeconds = 60;
+try
+{
+    Console.WriteLine("Creating subscription ...");
+    subscriberServiceApiClient.CreateSubscription(subscriptionName, topicName, pushConfig, ackDeadlineSeconds);
+    Console.WriteLine("Subscription created");
+}
+catch (Grpc.Core.RpcException)
+{
+    Console.WriteLine("Subscription already exists");
+}
+
+// Pull messages from the subscription
+SubscriberClient subscriber = await SubscriberClient.CreateAsync(subscriptionName);
+
+await subscriber.StartAsync((msg, cancellationToken) =>
+{
+    Console.WriteLine($"Received message {msg.MessageId} published at {msg.PublishTime.ToDateTime()}");
+    Console.WriteLine($"Text: '{msg.Data.ToStringUtf8()}'");
+    return Task.FromResult(SubscriberClient.Reply.Ack);
+});
+
+await subscriber.StopAsync(TimeSpan.FromSeconds(15));
